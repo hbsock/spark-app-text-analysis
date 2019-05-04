003daeac7e8615ffec77d620fbb4dd56978772da
Hanbin Sock <hbsock@gmail.com>
2019-05-04 12:08:26 -0400

    Update initial example code to read from novel text file.

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -2,13 +2,23 @@
 import org.apache.spark.sql.SparkSession
 
 object SimpleApp {
+
   def main(args: Array[String]) {
-    val logFile = "YOUR_SPARK_HOME/README.md" // Should be some file on your system
-    val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
-    val logData = spark.read.textFile(logFile).cache()
+    val input_text = "/home/hanbinsock/programman/haskell/web-scraper/output/a-will-eternal/chapter-0001.txt" // Should be some file on your system
+
+    val spark = SparkSession
+      .builder
+      .appName("Simple Application")
+      .config("spark.master", "local")
+      .getOrCreate()
+
+    val logData = spark.read.textFile(input_text).rdd.cache()
+
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
+
     println(s"Lines with a: $numAs, Lines with b: $numBs")
     spark.stop()
   }
+
 }
